ef80be337288a90cfac36f0364bd7656a3f1b696
Josh Dassinger <dassjosh@users.noreply.github.com>
2022-05-21 09:48:56 -0500

    Version++

--- src/Rust.DiscordSignLogger/Rust.DiscordSignLogger/Plugins/DiscordSignLogger.cs
+++ src/Rust.DiscordSignLogger/Rust.DiscordSignLogger/Plugins/DiscordSignLogger.cs
@@ -3,7 +3,7 @@ using Oxide.Plugins;
 namespace Rust.SignLogger.Plugins
 {
     //Define:FileOrder=1
-    [Info("Discord Sign Logger", "MJSU", "1.0.5")]
+    [Info("Discord Sign Logger", "MJSU", "1.0.6")]
     [Description("Logs Sign / Firework Changes To Discord")]
     public partial class DiscordSignLogger : RustPlugin
     {
